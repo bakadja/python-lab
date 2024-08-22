e904eeb0d7a0c7fbfeabed840845d22bba365c6f
Kevin Ngongang <contact@kevinpaulidor.de>
2024-08-22 19:21:55 +0200

    Famous Quote 2

--- 02-variables-and-data-types/famous-quote/main.py
+++ 02-variables-and-data-types/famous-quote/main.py
@@ -1,2 +1,4 @@
-quote = 'Albert Einstein once said, “A person who never made a mistake never tried anything new.”'
-print(quote)
+famous_person = "Albert Einstein"
+message = f'{famous_person} once said, “A person who never made a mistake never tried anything new.”'
+
+print(message)
