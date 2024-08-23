16e3dfd75eebaaaf42752b7401d4ca585d2348bc
Kevin Ngongang <contact@kevinpaulidor.de>
2024-08-23 11:54:37 +0200

    Guest List

---
+++ 03-lists/guest-list/main.py
@@ -0,0 +1,4 @@
+guests = ["stanislas","robert","oussama"]
+message = f"Dear {guests[0].title()},{guests[1].title()} and {guests[2].title()} I’m so happy to have you here tonight. Thank you for coming to share this dinner with me. Please make yourselves at home, and let’s enjoy this evening together!"
+
+print(message)
