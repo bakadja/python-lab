045478ea22ad26cc247b8f4e56a0131ac9dd65fc
Kevin Ngongang <contact@kevinpaulidor.de>
2024-08-23 17:21:56 +0200

    Seeing the World

---
+++ 03-lists/seeing-the-world/main.py
@@ -0,0 +1,26 @@
+locations = ["china", "france", "usa","italy","egypt"]
+
+print(locations)
+print()
+
+# temporaly sorted list
+print(sorted(locations))
+
+print()
+print(locations)
+
+#reverse methode
+locations.reverse()
+locations.reverse()
+print()
+print(locations)
+
+#sort methode
+locations.sort()
+print()
+print(locations)
+
+locations.sort(reverse=True)
+print()
+print(locations)
+
