8dc158b0ce312257c1175931b74e684a7f629b35
Kevin Ngongang <contact@kevinpaulidor.de>
2024-08-23 10:19:07 +0200

    Greetings

---
+++ 03-lists/greetings/main.py
@@ -0,0 +1,3 @@
+message = "Hello"
+names = ["john", "smith", "sebastien", "theo"]
+print(f"{message} {names[0].title()}\n{message} {names[1].title()}\n{message} {names[2].title()}\n{message} {names[3].title()}\n")
