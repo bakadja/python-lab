1fac118c1a9add001911abd6e722393ae27d6ffe
Kevin Ngongang <contact@kevinpaulidor.de>
2024-08-24 15:31:11 +0200

    Buffet

---
+++ 04-working-with-lists/buffet/main.py
@@ -0,0 +1,14 @@
+foods = ("general tso's chicken","fried rice","lo mein","crab rangoon","egg rolls")
+
+for food in foods:
+    print(f"{food.title()}")
+
+# raise an error with modify a tuple list item
+# foods[1] = "salad"
+
+# change foods menu
+foods = ("general tso's chicken","fried rice","lo mein","sweet and sour chicken","dim sum")
+print()
+for food in foods:
+    print(f"{food.title()}")
+
