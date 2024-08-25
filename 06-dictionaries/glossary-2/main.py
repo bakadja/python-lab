fee48392ceaa3f98caeb9d0669c604a5fea63b46
Kevin Ngongang <contact@kevinpaulidor.de>
2024-08-25 11:32:39 +0200

    Glossary-2

---
+++ 06-dictionaries/glossary-2/main.py
@@ -0,0 +1,31 @@
+glossary = {
+    "program": """A program is a set of instructions designed to perform 
+    a specific task or function on a computer. Examples include web browsers 
+    (like Chrome or Firefox),operating systems (such as Windows or macOS), 
+    development tools (like Visual Studio Code),and 
+    games (like Minecraft and Fortnite).""",
+    
+    "algorithm": """An algorithm is a step-by-step procedure 
+    or set of rules for solving a problem in programming. For instance, 
+    search engines like Google use algorithms to rank and 
+    retrieve information from extensive datasets.""",
+    
+    "debugging": """Debugging is the process of finding and fixing errors 
+    or bugs in a program. It involves steps like running the program, 
+    reviewing the code, inspecting variable values, and modifying code 
+    to address issues.""",
+    
+    "api": """(Application Programming Interface): An API defines rules, 
+    protocols, and tools for building software applications. 
+    It allows different software components to interact. You can use APIs 
+    to integrate third-party services or data into your application without 
+    building everything from scratch.""",
+    
+    "syntax": """In programming languages, syntax defines the rules 
+    for forming valid statements and expressions using symbols, keywords, 
+    and other elements. It ensures that the compiler or interpreter 
+    can understand the code""",
+}
+
+for key, value in glossary.items():
+    print(f"{key}:{value}\n")
