2ce0e141d2f38b7ae8ef6ebdad62b9fe7b5f66fa
Kevin Ngongang <contact@kevinpaulidor.de>
2024-08-25 10:12:07 +0200

    Person

---
+++ 06-dictionaries/person/main.py
@@ -0,0 +1,13 @@
+person = {
+    "first_name": "leonardo",
+    "last_name": "da vinci",
+    "age": 67,
+    "city": "vinci", 
+    "country": "italy",   
+}
+
+print(f"{person["first_name"].title()}")
+print(f"{person["last_name"].title()}")
+print(f"{person["age"]}")
+print(f"{person["city"].title()}")
+print(f"{person["country"].title()}")
