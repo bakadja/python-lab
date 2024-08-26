207fc8365cb43f5b54368071fea5fc5d7e3beef8
Kevin Ngongang <contact@kevinpaulidor.de>
2024-08-26 18:53:27 +0200

    Messages

---
+++ 08-functions/messages/main.py
@@ -0,0 +1,17 @@
+def show_messages(messages):
+    """ prints text message"""
+    for message in messages:
+        print(message)
+        print()
+
+positive_messages = [
+    "You're such an inspiration!",
+    "You're doing great!",
+    "Let me know if I can help in any way.",
+    "Your presence is a present to the world.",
+    "You've got this.",
+    "You are strong and can do this.",
+    "I'm here for you."
+]
+
+show_messages(positive_messages)
